e9a3c2a4c72145e3d719c2c96a86881154e5cc53
franco.zanini <francozanini1995@gmail.com>
2022-12-01 15:48:49 -0300

    day 01 part 2

--- src/day01/bin/day01.rs
+++ src/day01/bin/day01.rs
@@ -10,5 +10,5 @@ fn main() {
     println!("Result: {}", part1::part1(_input));
 
     println!("--- Part Two ---");
-    println!("Result: {}", part2::part2());
+    println!("Result: {}", part2::part2(_input));
 }
--- src/day01/bin/part2.rs
+++ src/day01/bin/part2.rs
@@ -1,15 +1,29 @@
 //! # Advent of Code - Day 1 - Part Two
 
-pub fn part2() -> usize {
-    return 0;
-}
+pub fn part2(input: &str) -> usize {
+    let lines = input.lines();
+    let mut fold = lines.fold(Vec::new(), |mut current, next| {
+        let len = current.len();
+        let to_add = match str::parse::<usize>(next) {
+            Ok(v) => v,
+            Err(_e) => 0,
+        };
 
-#[cfg(test)]
-mod day01 {
-    use super::*;
+        if next.trim() == "" || len == 0 {
+            current.push(if len == 0 { to_add } else { 0 });
+        } else {
+            current[len - 1] += to_add;
+        }
 
-    #[test]
-    fn test_part2() {
-        assert_eq!(part2(), 0);
-    }
+        return current;
+    });
+
+    fold.sort();
+
+    return fold
+        .into_iter()
+        .rev()
+        .take(3)
+        .reduce(|current, next| current + next)
+        .unwrap();
 }
