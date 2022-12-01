549ca08797642910108a868c282293ee8c6fb4f0
franco.zanini <francozanini1995@gmail.com>
2022-12-01 15:22:47 -0300

    day 01 part 1

--- src/day01/bin/day01.rs
+++ src/day01/bin/day01.rs
@@ -7,7 +7,7 @@ fn main() {
     let _input = include_str!("../input.txt");
 
     println!("--- Part One ---");
-    println!("Result: {}", part1::part1());
+    println!("Result: {}", part1::part1(_input));
 
     println!("--- Part Two ---");
     println!("Result: {}", part2::part2());
--- src/day01/bin/part1.rs
+++ src/day01/bin/part1.rs
@@ -1,15 +1,19 @@
 //! # Advent of Code - Day 1 - Part One
 
-pub fn part1() -> usize {
-    return 0;
-}
+pub fn part1(input: &str) -> usize {
+    let lines = input.lines();
+    let mut max = 0;
+    let mut current = 0;
+    lines.for_each(|next| {
+        if next.trim() == "" {
+            max = if current > max { current } else { max };
+            current = 0;
+        }
+        current += match str::parse::<usize>(next) {
+            Ok(v) => v,
+            Err(e) => 0,
+        };
+    });
 
-#[cfg(test)]
-mod day01 {
-    use super::*;
-
-    #[test]
-    fn test_part1() {
-        assert_eq!(part1(), 0);
-    }
+    return max;
 }
