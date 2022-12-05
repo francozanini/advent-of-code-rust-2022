b4ef8ab825abf523a5b5d33745fbe6394a201ee4
franco.zanini <francozanini1995@gmail.com>
2022-12-05 20:41:46 -0300

    refactor

--- src/day05/bin/day05.rs
+++ src/day05/bin/day05.rs
@@ -1,9 +1,7 @@
 //! # Advent of Code - Day 5
 
 use std::collections::VecDeque;
-
-mod part1;
-mod part2;
+use std::iter::{Enumerate, Filter, Map};
 
 fn transpose(v: Vec<Vec<&str>>) -> Vec<Vec<&str>> {
     return (0..v.iter().map(|row| row.len()).max().unwrap())
@@ -15,15 +13,40 @@ fn transpose(v: Vec<Vec<&str>>) -> Vec<Vec<&str>> {
         .collect();
 }
 
-fn main() {
-    let _input = include_str!("../input.txt");
-    let mut state = parse_crate_state(_input).clone();
-    let max = state.iter().map(|row| row.len()).max().unwrap() + 1;
-    let instructions = _input
-        .lines()
-        .enumerate()
-        .filter(|(i, _crates)| i.clone() > (max))
-        .map(|(_, instruction)| instruction);
+fn parse_crate_state(_input: &str) -> Vec<VecDeque<String>> {
+    let mut matrix: Vec<Vec<&str>> = vec![];
+    for line in _input.lines().take_while(|line| !line.contains("1")) {
+        matrix.push(line.split("").collect());
+    }
+
+    return transpose(matrix)
+        .into_iter()
+        .map(|row| {
+            row.into_iter()
+                .filter(|symbol| {
+                    !symbol.contains("[") && !symbol.contains("]") && !symbol.trim().is_empty()
+                })
+                .map(|symbol| symbol.to_string())
+                .collect::<VecDeque<String>>()
+        })
+        .filter(|row| !row.is_empty())
+        .collect();
+}
+
+fn print_result(final_state: Vec<VecDeque<String>>) -> Vec<String> {
+    let default = "".to_string();
+
+    final_state
+        .iter()
+        .map(|deque| deque.front().unwrap_or(&default).clone())
+        .collect::<Vec<String>>()
+}
+
+fn run_instructions(
+    initial_state: Vec<VecDeque<String>>,
+    instructions: Vec<&str>,
+) -> Vec<VecDeque<String>> {
+    let mut state = initial_state.clone();
 
     for instruction in instructions {
         let parsed_instruction: Vec<usize> = instruction
@@ -32,19 +55,8 @@ fn main() {
             .map(|n| n.parse().unwrap())
             .collect();
 
-        println!("before {:?}", state);
-
         let amount_to_move = parsed_instruction[0];
         let mut to_move = vec![];
-
-        println!("parsed {:?}", parsed_instruction);
-
-        println!(
-            "move {} from {} to {}",
-            amount_to_move,
-            parsed_instruction[1] - 1,
-            parsed_instruction[2] - 1
-        );
 
         for _ in 0..amount_to_move {
             let maybe_removed = state
@@ -63,46 +75,31 @@ fn main() {
                 .unwrap()
                 .push_front(item)
         }
-
-        println!("after {:?}", state);
     }
 
-    let default = "".to_string();
-
-    println!(
-        "{:?}",
-        state
-            .iter()
-            .map(|deque| deque.front().unwrap_or(&default))
-            .collect::<Vec<&String>>()
-    );
-
-    println!("--- Part One ---");
-    println!("Result: {}", part1::part1());
-
-    println!("--- Part Two ---");
-    println!("Result: {}", part2::part2());
+    return state;
 }
 
-fn parse_crate_state(_input: &str) -> Vec<VecDeque<String>> {
-    let mut matrix: Vec<Vec<&str>> = vec![];
-    for line in _input.lines().take_while(|line| !line.contains("1")) {
-        matrix.push(line.split("").collect());
-    }
-    println!("{:?}", matrix);
+fn find_instructions(_input: &str, max: usize) -> Vec<&str> {
+    _input
+        .lines()
+        .enumerate()
+        .filter(|(i, _crates)| i.clone() > (max))
+        .map(|(_, instruction)| instruction)
+        .collect()
+}
 
-    let transposed = transpose(matrix);
-    println!("{:?}", transposed);
-    return transposed
-        .into_iter()
-        .map(|row| {
-            row.into_iter()
-                .filter(|symbol| {
-                    !symbol.contains("[") && !symbol.contains("]") && !symbol.trim().is_empty()
-                })
-                .map(|symbol| symbol.to_string())
-                .collect::<VecDeque<String>>()
-        })
-        .filter(|row| !row.is_empty())
-        .collect();
+fn main() {
+    let _input = include_str!("../input.txt");
+    let state = parse_crate_state(_input).clone();
+    let max = state.iter().map(|row| row.len()).max().unwrap() + 1;
+    let instructions = find_instructions(_input, max);
+
+    let result_one = run_instructions(state, instructions);
+
+    println!("--- Part One ---");
+    println!("Result: {:?}", print_result(result_one));
+
+    println!("--- Part Two ---");
+    println!("Result: {}", "");
 }
